df474b58d8db990a9d96944306bcfaacb3037a39
lynnocu0us <115176580+lynnocu0us@users.noreply.github.com>
2022-11-22 21:42:02 -0600

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-function validateForm(){
-    var x = document.forms['googform']['field'].value;
-    if(x == null || x == "")
-    {
-        alert("please fill complete the form before submitting");
-        return false;
-    }
-}
